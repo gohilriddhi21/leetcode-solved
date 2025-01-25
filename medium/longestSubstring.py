ef5a9ad3215a3cccb3aa66781546bfe27cddbb3a
riddhi_gohil <gohilriddhi21@gmail.com>
2025-01-25 00:51:01 -0800

    longest substring

---
+++ medium/longestSubstring.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        i = j = max_len = 0
+        seen = set()
+        
+        while(i < n):
+            while seen and s[i] in seen:
+                seen.remove(s[j])
+                j += 1
+            seen.add(s[i])
+            max_len = max(max_len, i - j + 1)
+            i += 1    
+        return max_len
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.lengthOfLongestSubstring("abcabcbb"))
+    print(s.lengthOfLongestSubstring("bbbbb"))
+    print(s.lengthOfLongestSubstring("pwwkew"))
+    
+    
