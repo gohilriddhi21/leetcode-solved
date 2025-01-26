631a9e89dbcc13ebcadadda5550704db10a75618
riddhi_gohil <gohilriddhi21@gmail.com>
2025-01-25 17:02:54 -0800

    encode decode

--- neetcode/medium/encodeDecode.py
+++ neetcode/medium/encodeDecode.py
@@ -1,18 +1,28 @@
 class Solution:
-
     def encode(self, strs: list[str]) -> str:
         encoded = ""
-        for string in strs:
-            encoded = encoded + string + "#"
-        return encoded[:-1]
-
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
 
     def decode(self, s: str) -> list[str]:
-        res = s.split("#")
+        res = []
+        i = 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+            
         return res
 
 if __name__ == "__main__":
-    strs = ["a","b","c"]
+    strs = ["we","love","neetcode"]
     solution = Solution()
     print(solution.encode(strs))
     print(solution.decode(solution.encode(strs)))
