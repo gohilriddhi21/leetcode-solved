fb4747d689da1c93005e522b4bf31dac965aeef3
riddhi_gohil <gohilriddhi21@gmail.com>
2025-01-26 19:50:44 -0800

    binary search

--- neetcode/medium/twoIntegerSum.py
+++ neetcode/medium/twoIntegerSum.py
@@ -1,16 +1,14 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
         l, r = 0, len(numbers) - 1
-
         while l < r:
-            curSum = numbers[l] + numbers[r]
-
-            if curSum > target:
-                r -= 1
-            elif curSum < target:
+            currSum = numbers[l] + numbers[r]
+            if currSum < target:
                 l += 1
+            elif currSum > target:
+                r -= 1
             else:
-                return [l + 1, r + 1]
+                return [l+1, r+1]
         return []
                 
 
