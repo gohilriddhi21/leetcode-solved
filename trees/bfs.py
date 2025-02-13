43c189c9b8a27f3d272a8752967db43ca5d83da4
riddhi_gohil <gohilriddhi21@gmail.com>
2025-02-12 20:31:28 -0800

    bfs or level order

---
+++ trees/bfs.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> list[list[int]]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level_size = len(queue)
+            level_nodes = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_nodes.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level_nodes)
+
+        return result
+
+if __name__ == "__main__":
+    root = TreeNode(5)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    
+    root.right = TreeNode(8)
+    root.right.right = TreeNode(10)
+    root.right.left = TreeNode(6)
+    
+    print(Solution().levelOrder(root))
