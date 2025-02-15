a379c5fd303eb0d5d19bd9e477b858ec3c1ed443
riddhi_gohil <gohilriddhi21@gmail.com>
2025-02-14 16:51:03 -0800

    valid subdoku

---
+++ leetcode/medium/validSudoku.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        N = 9
+        # Use binary number to check previous occurrence
+        rows = [0] * N
+        cols = [0] * N
+        boxes = [0] * N
+
+        for r in range(N):
+            for c in range(N):
+                
+                # Check if the position is filled with number
+                if board[r][c] == ".":
+                    continue
+
+                pos = int(board[r][c]) - 1
+
+                # Check the row
+                if rows[r] & (1 << pos):
+                    return False
+                rows[r] |= 1 << pos
+
+                # Check the column
+                if cols[c] & (1 << pos):
+                    return False
+                cols[c] |= 1 << pos
+
+                # Check the box
+                idx = (r // 3) * 3 + c // 3
+                if boxes[idx] & (1 << pos):
+                    return False
+                boxes[idx] |= 1 << pos
+
+        return True
+
+if __name__ == "__main__":
+        board = [["5","5",".",".","7",".",".",".","."]
+                ,["6",".",".","1","9","5",".",".","."]
+                ,[".","9","8",".",".",".",".","6","."]
+                ,["8",".",".",".","6",".",".",".","3"]
+                ,["4",".",".","8",".","3",".",".","1"]
+                ,["7",".",".",".","2",".",".",".","6"]
+                ,[".","6",".",".",".",".","2","8","."]
+                ,[".",".",".","4","1","9",".",".","5"]
+                ,[".",".",".",".","8",".",".","7","9"]]
+        sol = Solution()
+        print(sol.isValidSudoku(board))
