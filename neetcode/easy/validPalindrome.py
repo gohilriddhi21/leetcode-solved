515601b57703b4efa01f32a0e6c32bf0eef8784e
riddhi_gohil <gohilriddhi21@gmail.com>
2025-03-23 22:58:16 -0700

    updates

--- neetcode/easy/validPalindrome.py
+++ neetcode/easy/validPalindrome.py
@@ -6,13 +6,13 @@ class Solution:
             if s[i].isalnum() and s[j].isalnum():
                 if s[i].lower() != s[j].lower():
                     return False
-                i+=1
-                j-=1
+                i += 1
+                j -= 1
             else:
                 if not s[i].isalnum():
-                    i+=1
+                    i += 1
                 if not s[j].isalnum():
-                    j-=1
+                    j -= 1
         return True
 
 if __name__ == "__main__":
