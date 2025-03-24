f99fcf5c55485a200b13c3344850bf6f5a663e32
riddhi_gohil <gohilriddhi21@gmail.com>
2025-03-23 22:32:13 -0700

    updates

--- sample-a/oddNumberTimes.py
+++ sample-a/oddNumberTimes.py
@@ -1,13 +1,9 @@
 from typing import Optional
 
 class Solution:
-    
     # O(n^2)
     def oddNumberTimes(self, nums: list[int]) -> Optional[int]:
         return next((i for i in nums if nums.count(i) % 2 != 0), None) 
-    
-    # O(n)
-    
 
 
 if __name__ == '__main__':
